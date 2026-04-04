6c267992a1b9ae24a6d03147c6d369a9c0b73d5f
SR-AndersoN <60825899+SR-AndersoN@users.noreply.github.com>
2026-04-04 21:44:22 +0800

    12

--- ultralytics/1.py
+++ ultralytics/1.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 # 【核心改动】
 # 1. 传入你魔改后的 yaml 配置文件名（如果报错找不到，可以写这个 yaml 的绝对路径）
 # 2. 加上 .load("yolov8n.pt") 来加载原版预训练权重
-model = YOLO("yolov8-ghost-ema.yaml").load("yolov8n.pt") 
+model = YOLO("yolov8-ghost-only.yaml").load("yolov8n.pt") 
 
 # 训练时直接传入数据增强参数
 results = model.train(
